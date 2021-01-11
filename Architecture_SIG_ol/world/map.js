e0f6ccdb2cf862428118ee4bf1e45fd229e41e56
JonathanCrt <jonathan.crete@outlook.fr>
2021-01-11 17:56:40 +0100

    [ADD] add select between diff sources (tiling or not)

--- Architecture_SIG_ol/world/map.js
+++ Architecture_SIG_ol/world/map.js
@@ -1,16 +1,16 @@
-let sketch;
-let helpTooltipElement;
-let helpToolTip;
-let measureTootltipElement;
-let measureTooltip;
-let continuePolygonMsg = 'Click to continue drawing the polygon';
-let continueLineMsg = 'Click to continue drawing the line';
-
-
-
 /*****************************************Layers sources *****************************************/
 
 const osmSource = new ol.source.OSM();
+let projection = ol.proj.get('EPSG:3857');
+let projectionExtent = projection.getExtent();
+var size = ol.extent.getWidth(projectionExtent) / 256;
+var resolutions = new Array(14);
+var matrixIds = new Array(14);
+for (var z = 0; z < 14; ++z) {
+  // generate resolutions and matrixIds arrays for this WMTS
+  resolutions[z] = size / Math.pow(2, z);
+  matrixIds[z] = z;
+}
 
 let layerRasterOSM = new ol.layer.Tile({
   title: 'OpenStreetMap',
@@ -19,9 +19,9 @@ let layerRasterOSM = new ol.layer.Tile({
   opacity: 1
 });
 
-let layerWMSCountries = new ol.layer.Image({
-  title: 'Countries',
-  visible: true,
+let layerWMSCountriesFromQgisServer = new ol.layer.Image({
+  title: 'Countries from Qgis Server',
+  visible: false,
   source: new ol.source.ImageWMS({
     url: 'http://localhost/qgisserver?',
     params: { 'LAYERS': 'countries' }
@@ -29,9 +29,40 @@ let layerWMSCountries = new ol.layer.Image({
   opacity: 0.4
 });
 
+let layerWMSCountriesFromQgisServerTiles = new ol.layer.Tile({
+  title: 'Countries from Qgis Server with tiling',
+  visible: false,
+  source: new ol.source.TileWMS({
+    url: 'http://localhost/qgisserver?',
+    params: { 'LAYERS': 'countries' }
+  }),
+  opacity: 0.4
+});
+
+let layerWMSCountriesFromMapProxy = new ol.layer.Image({
+  title: 'Countries from MapProxy',
+  visible: false,
+  source: new ol.source.ImageWMS({
+    url: 'http://localhost:8080/service?',
+    params: { 'LAYERS': 'countries' }
+  }),
+  opacity: 0.4
+});
+
+
+let layerWMSCountriesFromMapProxyTiles = new ol.layer.Tile({
+  title: 'Countries from MapProxy with tiling',
+  visible: false,
+  source: new ol.source.TileWMS({
+    url: 'http://localhost:8080/service?',
+    params: { 'LAYERS': 'countries' }
+  }),
+  opacity: 0.4
+});
+
 let layerWMSAirports = new ol.layer.Image({
   title: 'Airports',
-  visible: true,
+  visible: false,
   source: new ol.source.ImageWMS({
     url: 'http://localhost/qgisserver?',
     params: { 'LAYERS': 'airports' }
@@ -41,7 +72,7 @@ let layerWMSAirports = new ol.layer.Image({
 
 let layerWMSPlaces = new ol.layer.Image({
   title: 'Places',
-  visible: true,
+  visible: false,
   source: new ol.source.ImageWMS({
     url: 'http://localhost/qgisserver?',
     params: { 'LAYERS': 'places' }
@@ -49,26 +80,29 @@ let layerWMSPlaces = new ol.layer.Image({
   opacity: 1
 });
 
+
+let layerWMTSCountries =  new ol.layer.Tile ({
+  opacity: 0.7,
+  visible: true,
+  source: new ol.source.WMTS({
+    url: 'http://localhost:8080/service?',
+    params: { 'LAYERS': 'countries' },
+    matrixSet: 'EPSG:3857',
+    format: 'image/png',
+    projection: projection,
+    tileGrid: new ol.tilegrid.WMTS({
+        origin: ol.extent.getTopLeft(projectionExtent),
+        resolutions: resolutions,
+        matrixIds: matrixIds,
+    }),
+    style: 'default',
+    wrapX: true,
+  })
+}); 
+
+
+
 /***************************************** Extends functionnalities *****************************************/
-
-let pointerMoveHandler = (evt) => {
-  if (evt.dragging) {
-    return;
-  }
-  let helpMsg = 'Click to start drawing';
-  if (sketch) {
-    let geom = sketch.getGeometry();
-    if (geom instanceof Polygon) {
-      helpMsg = continuePolygonMsg;
-    } else if (geom instanceof LineString) {
-      helpMsg = continueLineMsg;
-    }
-  }
-  helpTooltipElement.innerHTML = helpMsg;
-  helpTooltip.setPosition(evt.coordinate);
-
-  helpTooltipElement.classList.remove('hidden');
-}
 
 let overviewMapControl = new ol.control.OverviewMap({
   layers: [
@@ -105,7 +139,16 @@ precisionInput.addEventListener('change', (event) => {
 
 let map = new ol.Map({
   target: 'map',
-  layers: [layerRasterOSM, layerWMSCountries, layerWMSAirports, layerWMSPlaces],
+  layers: [
+    layerRasterOSM, 
+    layerWMSCountriesFromQgisServer, 
+    layerWMSAirports, 
+    layerWMSPlaces, 
+    layerWMSCountriesFromMapProxy, 
+    layerWMSCountriesFromQgisServerTiles, 
+    layerWMSCountriesFromMapProxyTiles,
+    layerWMTSCountries
+  ],
   controls: ol.control.defaults().extend([mousePositionControl, overviewMapControl]),
   view: new ol.View({
     center: ol.proj.fromLonLat([2.5854809914, 48.8368466526]),
@@ -124,134 +167,56 @@ const layerSwitcher = new LayerSwitcher({
 map.addControl(layerSwitcher);
 
 
-map.on('pointermove', pointerMoveHandler);
+jQuery(() => {
+  $("#layerSelect").on('change', () => {
+    let selectValue = $("#layerSelect").val();
+    console.log(selectValue);
 
-map.getViewport().addEventListener('mouseout', () => {
-  helpTooltipElement.classList.add('hidden');
-});
+    const layers = [
+      layerWMSCountriesFromQgisServer,
+      layerWMSCountriesFromQgisServerTiles,
+      layerWMSCountriesFromMapProxy,
+      layerWMSCountriesFromMapProxyTiles
+    ];
 
-let typeSelect = document.getElementById('type');
+    function hideAllWMSLayers() {
+      layers.forEach((layer) => {
+        layer.setVisible(false);
+      });
+    }
 
-let draw;
-
-let formatLength = (line) => {
-  let length = getLength(line);
-  let output;
-  if (length > 100) {
-    output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
-  } else {
-    output = Math.round(length * 100) / 100 + ' ' + 'm';
-  }
-  return output;
-};
-
-let formatArea = (polygon) => {
-  let area = getArea(polygon);
-  let output;
-  if (area > 10000) {
-    output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
-  } else {
-    output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
-  }
-  return output;
-};
-
-function addInteraction() {
-  let type = typeSelect.value == 'area' ? 'Polygon' : 'LineString';
-  draw = new Draw({
-    source: source,
-    type: type,
-    style: new Style({
-      fill: new Fill({
-        color: 'rgba(255, 255, 255, 0.2)',
-      }),
-      stroke: new Stroke({
-        color: 'rgba(0, 0, 0, 0.5)',
-        lineDash: [10, 10],
-        width: 2,
-      }),
-      image: new CircleStyle({
-        radius: 5,
-        stroke: new Stroke({
-          color: 'rgba(0, 0, 0, 0.7)',
-        }),
-        fill: new Fill({
-          color: 'rgba(255, 255, 255, 0.2)',
-        }),
-      }),
-    }),
-  });
-  map.addInteraction(draw);
-
-  createMeasureTooltip();
-  createHelpTooltip();
-
-  let listener;
-  draw.on('drawstart', (evt) => {
-    // set sketch
-    sketch = evt.feature;
-
-    /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
-    let tooltipCoord = evt.coordinate;
-
-    listener = sketch.getGeometry().on('change', (evt) => {
-      let geom = evt.target;
-      let output;
-      if (geom instanceof Polygon) {
-        output = formatArea(geom);
-        tooltipCoord = geom.getInteriorPoint().getCoordinates();
-      } else if (geom instanceof LineString) {
-        output = formatLength(geom);
-        tooltipCoord = geom.getLastCoordinate();
-      }
-      measureTooltipElement.innerHTML = output;
-      measureTooltip.setPosition(tooltipCoord);
-    });
+    switch (selectValue) {
+      case "osm":
+        hideAllWMSLayers();
+        break;
+      case "world_qgis_single":
+        hideAllWMSLayers();
+        layerWMSCountriesFromQgisServer.setVisible(true);
+        break;
+      case "world_qgis_tiles":
+        hideAllWMSLayers();
+        layerWMSCountriesFromQgisServerTiles.setVisible(true);
+        break;
+      case "world_mapproxy_single":
+        hideAllWMSLayers();
+        layerWMSCountriesFromMapProxy.setVisible(true);
+        break;
+      case "world_mapproxy_tiles":
+        layers.forEach((layer) => {
+          layer.setVisible(false);
+        });
+        layerWMSCountriesFromMapProxyTiles.setVisible(true);
+        break;
+      default:
+        hideAllWMSLayers();
+        break;
+    }
   });
 
-  draw.on('drawend', function () {
-    measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
-    measureTooltip.setOffset([0, -7]);
-    // unset sketch
-    sketch = null;
-    // unset tooltip so that a new one can be created
-    measureTooltipElement = null;
-    createMeasureTooltip();
-    unByKey(listener);
-  });
-}
 
-function createHelpTooltip() {
-  if (helpTooltipElement) {
-    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
-  }
-  helpTooltipElement = document.createElement('div');
-  helpTooltipElement.className = 'ol-tooltip hidden';
-  helpTooltip = new Overlay({
-    element: helpTooltipElement,
-    offset: [15, 0],
-    positioning: 'center-left',
-  });
-  map.addOverlay(helpTooltip);
-}
 
-function createMeasureTooltip() {
-  if (measureTooltipElement) {
-    measureTooltipElement.parentNode.removeChild(measureTooltipElement);
-  }
-  measureTooltipElement = document.createElement('div');
-  measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
-  measureTooltip = new Overlay({
-    element: measureTooltipElement,
-    offset: [0, -15],
-    positioning: 'bottom-center',
-  });
-  map.addOverlay(measureTooltip);
-}
 
-typeSelect.onchange = () => {
-  map.removeInteraction(draw);
-  addInteraction();
-};
 
-addInteraction();
+
+
+})
